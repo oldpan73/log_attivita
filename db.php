158436a2e1f105682109bd640a7bb52bb5ecf24f
Andrea Salis <68701580+oldpan73@users.noreply.github.com>
2025-02-14 10:06:55 +0100

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $dbname = "test_log";
 $username = "root";
-$password = "andrea";
+$password = "****";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
